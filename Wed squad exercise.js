1bfbd532738168600defab886f8c21d2fac38992
eghak <88344325+eghak@users.noreply.github.com>
2021-08-11 06:52:38 -0700

    week 2 wed squad
    https://replit.com/@mckenjus/making-decisions-practice#index.js

---
+++ Wed squad exercise.js
@@ -0,0 +1,39 @@
+let myArr = [[1], [2], [3], [4], [5], [6]]; 
+
+for(i=0; i < myArr.length; i++) {
+  if(myArr[i] % 2 === 0) {
+    console.log(`${myArr[i]} is even`)
+  } else if (myArr[i] % 2 === 1) {
+    console.log(`${myArr[i]} is odd`)
+  }
+}
+
+ // for (i = 0; i < nums.length; i++) {
+  //   nums[i]
+  //   if i % 2 === 0{
+  //   evensArr.push(i)
+  // }
+  //   console.log(nums[i])
+  // }
+
+
+  //  for (i = 0; i < nums.length; i++) { 
+  //     if (nums[i] % 2 === 0) {
+  //       evensArr.push(nums[i])
+       
+  //     }
+  //   }
+
+  // console.log(nums)
+  // console.log(evensArr)
+
+// egha's code
+
+// for (i=0; i < myArr.length; i++ ) {
+//   if (myArr[i] % 2 === 0) {
+//     console.log(`${myArr[i]} is even`)
+//   } else if (myArr[i] % 1 === 0) {
+//     console.log(`${myArr[1]} is odd`)
+//   }
+// }
+
